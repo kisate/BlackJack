86d5e78a5fe256a5eeb857a10520330fadde3294
kisate <dhkas@mail.ru>
2016-11-09 17:05:13 +0300

    1

---
+++ src/com/company/Card.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by student3 on 09.11.16.
+ */
+public class Card {
+    public Utilities.Suits suit;
+    public Utilities.Values value;
+
+}
---
+++ src/com/company/Utilities.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by student3 on 09.11.16.
+ */
+public class Utilities {
+    public enum Suits {Spades, Hearts, Diamonds, Clubs}
+    public enum Values {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+}
